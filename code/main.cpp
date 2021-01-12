9ede48c5a2af35dfb02444f3db0012eedd9f375e
hooonfgfg <71239988+hooonfgfg@users.noreply.github.com>
2021-01-12 19:01:43 +0900

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,19 +1,24 @@
-﻿#include "BaseCal.h"
+#include "BaseCal.h"
 #include "PostFix.h"
 #include "MyCal.h"
 #include "MyGUICalc.h"
 #include <tchar.h>
+
+int calc(BaseCal* basecal,std::string str)
+{
+	return basecal->Calculation(str);
+}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
 	BaseCal *basecal;
 	
 	basecal = new MyCal;
-	int resultMyCal = basecal->Calculation("3+6+4/2");
+	std::cout<<calc(basecal, "3+6+4/2")<<std::endl;
 	delete basecal;
 	
 	basecal = new PostFix;
-	int resultPostfix = basecal->Calculation("(2*(3+6/2)+2)/4+33+3+2*3");
+	std::cout<<calc(basecal, "(2*(3+6/2)+2)/4+33+3+2*3")<<std::endl;
 	delete basecal;
 
 	return 0;
